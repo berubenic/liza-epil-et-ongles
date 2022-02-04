a3a840e347c4dcdc2c83075487180a04fda0006c
berubenic <berubenic@gmail.com>
2022-02-04 07:49:24 -0500

    Component added: DesktopNav,  responsive nav for large screens

---
+++ src/components/desktop-nav.jsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { Link, useTranslation, useI18next } from "gatsby-plugin-react-i18next"
+import * as style from "./header.module.css"
+
+const DesktopNav = () => {
+  const { languages, originalPath } = useI18next()
+  const { t } = useTranslation("header")
+  return (
+    <nav className={style.nav}>
+      <ul className={style.navList}>
+        <li>
+          <Link to="/">{t("home")}</Link>
+        </li>
+        <li>
+          <Link to="#">{t("services")}</Link>
+        </li>
+        <li>
+          <Link to="#">{t("gallery")}</Link>
+        </li>
+        <li>
+          <Link to="#">{t("blog")}</Link>
+        </li>
+        <li>
+          <Link to="#">{t("contact")}</Link>
+        </li>
+      </ul>
+      <ul className={style.languages}>
+        {languages.map(lng => (
+          <li key={lng}>
+            <Link to={originalPath} language={lng}>
+              {lng}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default DesktopNav
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link, useTranslation, useI18next } from "gatsby-plugin-react-i18next"
-import { elastic as Menu } from "react-burger-menu"
 import * as style from "./header.module.css"
 import MobileNav from "./mobile-nav"
+import DesktopNav from "./desktop-nav"
 import { Media } from "gatsby-plugin-fresnel"
 
 const Header = ({ siteTitle, pageWrapId, outerContainerId }) => {
@@ -13,11 +13,14 @@ const Header = ({ siteTitle, pageWrapId, outerContainerId }) => {
       <h1 className={style.companyName}>
         <Link to="/">{siteTitle}</Link>
       </h1>
-      <Media at="sm">
+      <Media lessThan="lg">
         <MobileNav
           pageWrapId={pageWrapId}
           outerContainerId={outerContainerId}
         />
+      </Media>
+      <Media greaterThanOrEqual="lg">
+        <DesktopNav />
       </Media>
     </header>
   )
