41aec80b06b148c572b9a4b911588188a5197791
berubenic <berubenic@gmail.com>
2022-02-12 14:57:03 -0500

    Header desktop style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,24 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import logo from "../images/logo.svg"
-import { Link, useTranslation } from "gatsby-plugin-react-i18next"
-import * as style from "./header.module.css"
+import { Link } from "gatsby-plugin-react-i18next"
+import { StaticImage } from "gatsby-plugin-image"
+import { companyContainer, companyName, companyLogo } from "./header.module.css"
 import MobileNav from "./mobile-nav"
 import DesktopNav from "./desktop-nav"
 import { Media } from "gatsby-plugin-fresnel"
 
 const Header = () => {
-  const { t } = useTranslation("header")
   return (
     <header>
-      <div className={style.companyContainer}>
-        <img src={logo} alt={t("logo-alt")} className={style.companyLogo} />
+      <div className={companyContainer}>
+        <StaticImage
+          src="../images/logo.svg"
+          alt=""
+          width={80}
+          className={companyLogo}
+        />
         <h1>
-          <Link to="/" className={style.companyName}>
+          <Link to="/" className={companyName}>
             Épil et Ongles
           </Link>
         </h1>
