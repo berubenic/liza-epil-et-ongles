cb5ec19314c35b00f75ebb9910ca7854e9c64cae
berubenic <berubenic@gmail.com>
2022-02-13 15:58:17 -0500

    Remove url from image src SEO'

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -25,7 +25,7 @@ const SEO = ({ title, description, image, article, language }) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: `${siteUrl}${image || defaultImage}`,
+    image: image || defaultImage,
     url: `${siteUrl}${pathname}`,
   }
 
