4aaec0de968233dfdcfa361b8cb7378935be0c67
Nicholas Berube <berubenic@gmail.com>
2022-06-05 14:53:44 -0400

    Add Leaflet map to home page.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,14 @@ import { Media } from "gatsby-plugin-fresnel"
 import {
   introContainer,
   serviceBannerContainer,
+  mapContainer
 } from "./index.module.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import ServiceBanner from "../components/service-banner"
 import ContactBannerMobile from "../components/contact-banner-mobile"
 import ContactBannerDesktop from "../components/contact-banner-desktop"
+import MyMap from "../components/my-map"
 import { graphql } from "gatsby"
 
 const IndexPage = ({ data }) => {
@@ -43,6 +45,18 @@ const IndexPage = ({ data }) => {
       <Media greaterThanOrEqual="lg">
         <ContactBannerDesktop></ContactBannerDesktop>
       </Media>
+        <div className={mapContainer}>
+          {typeof window !== 'undefined' &&
+            <MyMap
+              position={[45.88645678865315, -73.280724539847]}
+              zoom={14}
+              markerTextLine1={t("markerTextLine1")}
+              textBoxLine1={t("textBoxLine1")}
+              textBoxGoogleMapLink={"https://www.google.com/maps/place/Li+-+%C3%89pil+et+Ongles/@45.8863777,-73.2804985,21z/data=!4m5!3m4!1s0x0:0xa35734e304599d02!8m2!3d45.886429!4d-73.2807312"}
+              textBoxLinkText={t("textBoxLinkText")}
+            />
+          }
+        </div>
     </Layout>
   )
 }
