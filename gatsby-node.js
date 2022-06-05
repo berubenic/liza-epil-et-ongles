a9be89838d536ae1154fcefad1be8c0e866cc295
Nicholas Berube <berubenic@gmail.com>
2022-06-05 16:06:01 -0400

    fix for window is not defined in build.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,15 @@
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html" || stage === "develop-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /leaflet/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
